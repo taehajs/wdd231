24a9f1259fbff98ae4f99eea2baded5085929e26
taehajs <taehajs@naver.com>
2025-12-02 19:21:39 +0900

    asd

--- Final/scripts/utils.js
+++ Final/scripts/utils.js
@@ -1,7 +1,7 @@
-export async function fetchJSON(path) {
+export async function fetchJSON(url) {
   try {
-    const res = await fetch(path);
-    if (!res.ok) throw new Error("Failed to fetch JSON");
+    const res = await fetch(url);
+    if (!res.ok) throw new Error('Network response was not ok');
     return await res.json();
   } catch (err) {
     console.error(err);
